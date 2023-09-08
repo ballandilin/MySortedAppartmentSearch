b425c7acbadbdf97359efb12f4f2e35b5a32f832
ballandilin <nicolas.benoit6@gmail.com>
2023-09-08 09:54:09 +0200

    remove uto folder

--- scrapImmo.py
+++ scrapImmo.py
@@ -7,23 +7,32 @@ class ImmoScrap(scrapy.Spider):
     start_urls = ['https://www.seloger.com/list.htm?projects=1&types=2,1&places=[{"inseeCodes":[210292]},{"inseeCodes":[210231]}]&price=NaN/550&surface=40/NaN&rooms=2,3&mandatorycommodities=0&enterprise=0&qsVersion=1.0&sort=d_dt_crea&m=search_hp_last']
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'FEED_EXPORT_ENCODING' : 'utf-8'
     }
     
     
     def parse(self, response):
-        for article in response.css('div[data-testid="sl.explore.card-container"]'):
-            title = article.css("div[data-test='sl.title']::text").get()
+        items = {}
+        for i, article in enumerate(response.css('div[data-testid="sl.explore.card-container"]')):
+            lien = "https://www.seloger.com" + article.css("a[data-testid='sl.explore.coveringLink']::attr(href)").get()
+            typeBien = article.css("div[data-test='sl.title']::text").get()
             prix = article.css("div[data-test='sl.price-label']::text").get()
             description = article.css("div[data-testid='sl.explore.card-description']::text").get()
+            localisation = article.css("div[data-testid='sl.address']::text").get()
+            specificite = [spe for spe in article.css("li::text").getall()]
 
-            item = {
-                'title': title,
+            items[i] = {
+                'lien': lien,
+                'type' : typeBien,
                 'prix': prix,
+                'localisation' : localisation,
+                'specificite' : specificite,
                 'description' : description,
+                'VALIDE' : None
             }
             
-            with open("res.json", "a") as f:
-                json.dump(item, f)
+        with open("res.json", "w", encoding="utf-8") as f:
+            json.dump(items, f)
 
         next_page = response.css('a.next::attr(href)').get()
         if next_page is not None:
